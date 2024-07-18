f2c13a60ec38abb15fc2fbf2475637e7022011e6
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-07-18 15:37:41 -0300

    feat: add user check-ins history service

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -38,4 +38,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkInOnSameDate;
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    const userCheckIns = this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+
+    return userCheckIns;
+  }
 }
