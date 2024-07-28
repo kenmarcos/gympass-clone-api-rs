f8eda16f4f0a0820e2475f85b5890b51fc6ff5fa
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-07-28 16:17:11 -0300

    feat: add get user profile controller

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from "fastify";
 import { register } from "./users/register";
 import { authenticate } from "./users/authenticate";
 import { profile } from "./users/profile";
+import { verifyJWT } from "@/middlewares/verify-jwt";
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.post("/users", register);
 
-  app.get("/me", profile);
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 
   app.post("/sessions", authenticate);
 };
--- src/controllers/users/profile.ts
+++ src/controllers/users/profile.ts
@@ -1,7 +1,19 @@
+import { makeGetUserProfile } from "@/services/factories/make-get-user-profile-service";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export const profile = async (request: FastifyRequest, reply: FastifyReply) => {
   await request.jwtVerify();
 
-  return reply.status(200).send();
+  const getUserProfileService = makeGetUserProfile();
+
+  const { user } = await getUserProfileService.execute({
+    userId: request.user.sub,
+  });
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 };
